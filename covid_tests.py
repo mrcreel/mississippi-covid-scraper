0d56b4cbde5a8f5622abdafe85e8f53187a73fe6
mrcreel <michaelrcreel@gmail.com>
2020-04-18 14:02:18 -0500

    Added tests date to data file

--- covid_tests.py
+++ covid_tests.py
@@ -8,13 +8,10 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-updated_on = ""
-"""
 raw_totals_date = soup.find(
     id='assetNow_pageSubtitle').text.split('Updated ')[1]
 updated_strp = datetime.strptime(raw_totals_date, '%B %d, %Y')
 updated_on = updated_strp.strftime('%Y-%m-%d')
-"""
 
 cases = soup.find_all(class_="simpleTable")[-1]
 
@@ -27,12 +24,12 @@ msdh_total_tests = int(''.join(msdh_total_tests_raw))
 commercial_tests_raw = tests[1].find_all('td')[1].text.split(',')
 commercial_tests = int(''.join(commercial_tests_raw))
 
-testing = [msdh_total_tests, commercial_tests,
+testing = [updated_on, msdh_total_tests, commercial_tests,
            msdh_total_tests+commercial_tests]
 
 print(testing)
 
 # Write to csv
-with open("covid_tests"+updated_on+".csv", "w", newline="") as f:
+with open("data/covid_tests_"+updated_on+".csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(testing)
