43b7538f87557fb8a1a69a3b48fd3fb07166eb21
mrcreel <michaelrcreel@gmail.com>
2020-04-18 17:12:14 -0500

    Add MPHL tests

--- covid_tests.py
+++ covid_tests.py
@@ -17,15 +17,21 @@ cases = soup.find_all(class_="simpleTable")[-1]
 
 tests = cases.find_all('tr')
 
+
 msdh_total_tests_raw = tests[0].find_all('td')[1].text.split(',')
 msdh_total_tests = int(''.join(msdh_total_tests_raw))
-
 
 commercial_tests_raw = tests[1].find_all('td')[1].text.split(',')
 commercial_tests = int(''.join(commercial_tests_raw))
 
+uls = soup.find_all('ul')
+mphl = uls[14]
+mphl_data = mphl.find_all('li')
+mphl_tests = mphl_data[0].text.split(': ')[1].split(',')
+mphl_tests = int(''.join(mphl_tests))
+
 testing = [updated_on, msdh_total_tests, commercial_tests,
-           msdh_total_tests+commercial_tests]
+           msdh_total_tests+commercial_tests, mphl_tests]
 
 print(testing)
 
